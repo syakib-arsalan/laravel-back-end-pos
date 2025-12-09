c344acf973b5614b47293c74d3b00168ea06f275
Syakib-Arsalan74 <syakibgithub@gmail.com>
2025-12-09 20:57:44 +0700

    penambahan model, migration, request, relasi untuk employee

---
+++ database/migrations/2025_12_09_123635_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->string('position');
+            $table->decimal('salary', 15, 2);
+            $table->date('hire_date');
+            $table->date('date_of_birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
