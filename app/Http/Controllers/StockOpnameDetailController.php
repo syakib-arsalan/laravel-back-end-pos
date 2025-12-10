3b9326651a1ad1eede53a6dc939c65f326b6b4f0
Syakib-Arsalan74 <syakibgithub@gmail.com>
2025-12-10 14:49:54 +0700

    penambahan model Stock Opname Detail

---
+++ app/Http/Controllers/StockOpnameDetailController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StockOpnameDetail;
+use App\Http\Requests\StoreStockOpnameDetailRequest;
+use App\Http\Requests\UpdateStockOpnameDetailRequest;
+
+class StockOpnameDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStockOpnameDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StockOpnameDetail $stockOpnameDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StockOpnameDetail $stockOpnameDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStockOpnameDetailRequest $request, StockOpnameDetail $stockOpnameDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StockOpnameDetail $stockOpnameDetail)
+    {
+        //
+    }
+}
