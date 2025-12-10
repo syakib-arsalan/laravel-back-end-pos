1b61420416b6b3d83e41b5dce51f9fb40b799b5a
Syakib-Arsalan74 <syakibgithub@gmail.com>
2025-12-10 16:47:55 +0700

    inisialisasi model Order

---
+++ database/migrations/2025_12_10_094244_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->string('order_type');
+            $table->dateTime('order_date');
+            $table->decimal('total_amount');
+            $table->string('status');
+            $table->string('payment_method');
+            $table->string('payment_status');
+            $table->string('shipping_address');
+            $table->string('shipping_city');
+            $table->string('shipping_state');
+            $table->string('shipping_postal_code');
+            $table->string('shipping_country');
+            $table->decimal('discount');
+            $table->decimal('tax');
+            $table->string('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
